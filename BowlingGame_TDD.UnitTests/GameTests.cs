cdb199fece6f09a33626fd6a090122ae1dc16d45
Julian-Chu <nightmare0107@gmail.com>
2016-02-04 10:28:56 +0100

    Added First test: roll 20 times Gutter

---
+++ BowlingGame_TDD.UnitTests/GameTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BowlingGame_TDD;
+using NUnit.Framework;
+namespace BowlingGame_TDD.UnitTests
+{
+    [TestFixture()]
+    public class GameTests
+    {
+        Game game;
+        [Test()]
+        public void Game_Roll20timesGutter_Returns0()
+        {
+            //Arrange
+            game = new Game();
+            int expected = 0;
+            int actual;
+            //Act
+            for(int i=0;i<20;i++)
+            {
+                game.Roll(0);
+            }
+            actual = game.CalculatingScore();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+    }
+}
